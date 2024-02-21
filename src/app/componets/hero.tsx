510fb9f21f9f10570c0253e1c15b0563d883d853
Nikita Starkov <lucasudar@gmail.com>
2024-02-21 14:43:44 +0700

    fixed path

--- src/app/componets/hero.tsx
+++ src/app/componets/hero.tsx
@@ -54,7 +54,7 @@ const Hero = () => {
         <Image
           className="z-[-1]"
           key={index}
-          src="/assets/bean.png"
+          src="/assets/img/bean.png"
           alt="bean"
           width="100"
           height="100"
